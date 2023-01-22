f9b55d3b5ef449ec3307c6a2cca785656930b125
Pasha Baran <pashabaran1207@gmail.com>
2023-01-23 00:29:34 +0200

    Add ttl after which the element is auto deleteв

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"errors"
 	"sync"
+	"time"
 )
 
 // Cache is a struct for storage any value by unique key
@@ -19,10 +20,16 @@ func New() *Cache {
 }
 
 // Set add to storage new value by key
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
 	c.mu.Lock()
 	c.data[key] = value
 	c.mu.Unlock()
+
+	// Auto-delete after a specified duration
+	go func() {
+		time.Sleep(ttl)
+		_ = c.Delete(key)
+	}()
 }
 
 // Get return stored value by key if it exists
@@ -45,8 +52,7 @@ func (c *Cache) Delete(key string) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	_, exist := c.data[key]
-	if !exist {
+	if _, exist := c.data[key]; !exist {
 		return errors.New("delete: unknown key")
 	}
 	delete(c.data, key)
