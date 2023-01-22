0e54c3e07be1f59067718603534d2e3c24213a6e
Pasha Baran <pashabaran1207@gmail.com>
2023-01-22 14:28:51 +0200

    Eliminate possible race condition by adding mutex

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,9 +1,13 @@
 package cache
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 // Cache is a struct for storage any value by unique key
 type Cache struct {
+	mu   sync.RWMutex
 	data map[string]interface{}
 }
 
@@ -16,12 +20,17 @@ func New() *Cache {
 
 // Set add to storage new value by key
 func (c *Cache) Set(key string, value interface{}) {
+	c.mu.Lock()
 	c.data[key] = value
+	c.mu.Unlock()
 }
 
 // Get return stored value by key if it exists
 // Otherwise, returns an error
 func (c *Cache) Get(key string) (interface{}, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
 	value, exist := c.data[key]
 	if !exist {
 		return nil, errors.New("get: unknown key")
@@ -33,10 +42,14 @@ func (c *Cache) Get(key string) (interface{}, error) {
 // Delete remove value from storage by key if it exists
 // Otherwise, returns an error
 func (c *Cache) Delete(key string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	_, exist := c.data[key]
 	if !exist {
 		return errors.New("delete: unknown key")
 	}
 	delete(c.data, key)
+
 	return nil
 }
