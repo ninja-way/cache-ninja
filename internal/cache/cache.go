154ff5427aa70bff4eb63e24da6b22930496e58b
Pasha Baran <pashabaran1207@gmail.com>
2023-01-22 09:18:46 +0200

    Add comments for struct and all methods

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -2,20 +2,25 @@ package cache
 
 import "errors"
 
+// Cache is a struct for storage any value by unique key
 type Cache struct {
 	data map[string]interface{}
 }
 
+// New initialize and return cache pointer
 func New() *Cache {
 	return &Cache{
 		data: make(map[string]interface{}),
 	}
 }
 
+// Set add to storage new value by key
 func (c *Cache) Set(key string, value interface{}) {
 	c.data[key] = value
 }
 
+// Get return stored value by key if it exists
+// Otherwise, returns an error
 func (c *Cache) Get(key string) (interface{}, error) {
 	value, exist := c.data[key]
 	if !exist {
@@ -25,6 +30,8 @@ func (c *Cache) Get(key string) (interface{}, error) {
 	return value, nil
 }
 
+// Delete remove value from storage by key if it exists
+// Otherwise, returns an error
 func (c *Cache) Delete(key string) error {
 	_, exist := c.data[key]
 	if !exist {
--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -2,6 +2,7 @@ package cache
 
 import "github.com/ninja-way/cache-ninja/internal/cache"
 
+// New return cache pointer
 func New() *cache.Cache {
 	return cache.New()
 }
